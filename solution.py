48de910968b0d9dedb0d397eed529db2280bbb52
khotiashova7 <74974781+khotiashova7@users.noreply.github.com>
2020-11-24 18:05:55 +0300

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,17 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('proekt.ui', self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
